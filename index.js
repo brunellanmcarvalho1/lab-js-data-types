4900e333e9a4c21580f4cf84b0ddc6c7b5ff60de
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2024-10-17 11:53:02 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s1} ${s2} ${s3} ${s4}`;
 
 // Print out the concatenated string
 
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,12 +21,14 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const newPart1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const newPart2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
 
+constcamelTail = newPart1 + newPart2;
 
 // Print the cameLtaiL-formatted string
 
-
-
+console.log(constcamelTail);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -36,11 +37,10 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
-
+const tipAmount = billTotal * 0.15;
 // Print out the tipAmount
 
-
-
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,11 +48,10 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
-
-
-
+console.log(randomNumber);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -62,15 +61,30 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// False
 
 const expression2 = a || b;
+// True
 
 const expression3 = !a && b;
+// False
 
 const expression4 = !(a && b);
+// True
 
 const expression5 = !a || !b;
+// True
 
 const expression6 = !(a || b);
+// False
 
 const expression7 = a && a;
+// True
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
